e3b234bbfa777cf2aa266d69103f99014b221713
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-02 12:06:52 +0530

    data is dinamic now

--- src/components/Home/FoodItem.jsx
+++ src/components/Home/FoodItem.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import foodIMG from "../../assets/foodimg.jpg";
 import { Box } from "@mui/material";
 
-function FoodItem() {
+function FoodItem(props) {
   return (
     <Box className="food-item" border="2px solid" padding="10px">
       <Box display="flex" justifyContent="center">
-        <img src={foodIMG} alt="food"/>
+        <img src={props.img} alt="food"/>
       </Box>
-      <h3>Chilli Panner</h3>
+      <h3>{props.name}</h3>
       <Box className="row">
         <Box>
         <select name="cars" id="item">
-          <option value="volvo">1</option>
-          <option value="saab">2</option>
-          <option value="mercedes">3</option>
-          <option value="audi">4</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
         </select>
         </Box>
         <Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "../components/Home/Hero";
 import { Box } from "@mui/material";
 import Head from "../components/Home/Head";
 import FoodItem from "../components/Home/FoodItem";
 import Footer from "../components/Footer";
+import axios from 'axios';
 
 function Home() {
+  const [foodItem, setFoodItem] = useState([]);
+
+  axios.get('http://localhost:5000/api/v1/items/list')
+  .then(response => {
+    setFoodItem(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
   return (
     <Box>
       <Hero />
       <Head />
       <Box className="fooditem-grid" mx={5}>
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
+        {
+          foodItem !==[]
+          ? foodItem.map((data)=>{
+            return (<div><FoodItem name={data.name} img={data.img} /></div>)
+          }): <div>Loading....</div>
+        }
+        
       </Box>
       <Box display="block">
         <Footer />
