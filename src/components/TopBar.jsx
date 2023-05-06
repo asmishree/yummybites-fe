5b85f4fed0a266cc45c9e8c6a4e7976421629563
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-06 16:23:12 +0530

    Update TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
-import { Box,  useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../theme";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from "@mui/icons-material/Menu";
 import Badge from '@mui/material/Badge';
 import "./TopBar.css";
@@ -12,31 +13,12 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useCart } from "./ContextReducer";
 import { toast } from "react-hot-toast";
 
-// Mob NavBar Work
-
-import Menu from '@mui/material/Menu';
-
-
-
 const Topbar = () => {
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  // Mob NavBar Work
-
-
-
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const [navbar, setNavbar] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const authToken = (localStorage.getItem("authToken"));
   const navigate = useNavigate();
   let data = useCart()
@@ -60,7 +42,6 @@ const Topbar = () => {
       color: navbar ? colors.link[500] : "white",
       borderBottom: isActive ? "2px solid" : "none",
       textDecoration: "none",
-      fontSize:"20px"
     };
   };
 
@@ -73,6 +54,10 @@ const Topbar = () => {
       fontWeight:"500",
       margin:"10px 0"
     };
+  };
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
   };
 
   const changeBackground = () => {
@@ -108,15 +93,15 @@ const Topbar = () => {
               <Box display="flex">
                 <NavLink style={navLinkStyles} to="/cart">
                 <Badge badgeContent={data.length} color="warning" overlap="circular">
-                  <ShoppingCartIcon sx={{ fontSize: 30 }}/>
+                  <ShoppingCartIcon/>
                   </Badge>
                 </NavLink>
                 <NavLink style={navLinkStyles} to="/order">
                   <span>MY ORDERS</span>
                 </NavLink>
-                <NavLink style={navLinkStyles} to="/login">
+                <Box>
                   <span onClick={handleLogout}>LOGOUT</span>
-                </NavLink>
+                </Box>
               </Box>
             ) : (
               <Box>
@@ -133,35 +118,26 @@ const Topbar = () => {
               mx={1}
             >
               {theme.palette.mode === "dark" ? (
-                <DarkModeOutlinedIcon sx={{ cursor: "pointer",fontSize: 30  }} />
+                <DarkModeOutlinedIcon sx={{ cursor: "pointer" }} />
               ) : (
-                <LightModeOutlinedIcon sx={{ cursor: "pointer",fontSize: 30  }} />
+                <LightModeOutlinedIcon sx={{ cursor: "pointer" }} />
               )}
-              
             </Box>
-
-            <div>
-      <Box
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-        color={navbar ? "" : "white"}
-        className="mobMenu"
-      >
-        <MenuIcon sx={{ cursor: "pointer",fontSize: 30  }} />
+            <Box className="mobMenu">
+              <MenuIcon onClick={handleMenuClick} />
+            </Box>
+          </Box>
+        </Box>
       </Box>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
+      <Box
+        className={menuOpen ? "mob-navbar open" : "mob-navbar"}
+        bgcolor={colors.primary[400]}
       >
-        <Box width={"100vw"} py={"20px"} >
+        <Box textAlign="right">
+          <IconButton onClick={handleMenuClick}>
+            <CloseIcon fontSize="large"/>
+          </IconButton>
+        </Box>
         <Box
           display="flex"
           flexDirection="column"
@@ -197,14 +173,7 @@ const Topbar = () => {
             )}
     
         </Box>
-        </Box>
-      </Menu>
-    </div>
-
-          </Box>
-        </Box>
       </Box>
-
     </Box>
   );
 };
